fbc02913739a80c16f9bac8209dc87a2456245c9
sankkokk <118100738+sankkokk@users.noreply.github.com>
2023-06-04 19:35:23 +0500

    Update and rename zadanie_2_laba10 to zadanie_2_laba10.java

---
+++ zadanie_2_laba10.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class zadanie_2_laba10 {
+    public static void main(String[] args) throws IOException {
+        File file1 = new File("C:\\Users\\muzbe\\OneDrive\\Рабочий стол\\aboba\\files\\t2.txt");
+        FileWriter fwr = new FileWriter(file1);
+        String[][] array = table(9, 9);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                sb.append(i + 1).append(" * ").append(j + 1).append(" = ").append(array[i][j]).append("\t");
+            }
+            fwr.write(String.valueOf(sb) + "\n");
+            sb = new StringBuilder();
+        }
+        fwr.flush();
+        fwr.close();
+    }
+    public static String[][] table(int lines, int columns){
+        String[][] array = new String[lines][columns];
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                array[i][j] = String.valueOf((i + 1)*(j + 1));
+            }
+        }
+        return array;
+    }
+}
